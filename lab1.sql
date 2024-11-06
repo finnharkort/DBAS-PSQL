70dcf6365e62ee4f2296e6149068bd6503cac62b
KickoKicko <kickokicko110@gmail.com>
2024-11-06 13:39:21 +0100

    All tables

--- lab1.sql
+++ lab1.sql
@@ -1,36 +1,106 @@
-DROP TABLE smuser;
-DROP TABLE textPost;
+DROP TABLE IF EXISTS smuser;
+DROP TABLE IF EXISTS textPost;
+DROP TABLE IF EXISTS TextPost;
+DROP TABLE IF EXISTS Post;
+DROP TABLE IF EXISTS ImagePost;
+DROP TABLE IF EXISTS videoPost;
 
-CREATE TABLE sm_user(
+CREATE TABLE users(
     userID INT PRIMARY KEY,
     fullName VARCHAR(255)
 );
 
-CREATE TABLE Post(
+CREATE TABLE post(
     postID INT PRIMARY KEY,
     userID INT NOT NULL,
     title VARCHAR(255),
     date DATE NOT NULL,
     place VARCHAR(255),
     tag VARCHAR(255)[],
-    CONSTRAINT fk_sm_user
+    CONSTRAINT fk_users
         FOREIGN KEY (userID)
-            REFERENCES sm_user (userID)
+            REFERENCES users (userID)
 );
 
-CREATE TABLE TextPost(
+CREATE TABLE textPost(
     postID INT PRIMARY KEY,
     textContent VARCHAR(255) NOT NULL
+    CONSTRAINT fk_post
+        FOREIGN KEY (postID)
+            REFERENCES Post (postID)
 );
 
-CREATE TABLE ImagePost(
+CREATE TABLE imagePost(
     postID INT PRIMARY KEY,
     imageURL VARCHAR(255) NOT NULL,
-    filter VARCHAR(255)[]
+    filter VARCHAR(255)[],
+    CONSTRAINT fk_post
+        FOREIGN KEY (postID)
+            REFERENCES Post (postID)
 );
 
-CREATE TABLE VideoPost(
+CREATE TABLE videoPost(
     postID INT PRIMARY KEY,
     videoURL VARCHAR(255) NOT NULL,
-    codec VARCHAR(255)
+    codec VARCHAR(255),
+    CONSTRAINT fk_post
+        FOREIGN KEY (postID)
+            REFERENCES Post (postID)
+);
+
+CREATE TABLE like(
+    postID INT PRIMARY KEY,
+    userID INT PRIMARY KEY,
+    timestamp VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_post
+        FOREIGN KEY (postID)
+            REFERENCES Post (postID),
+    CONSTRAINT fk_users
+        FOREIGN KEY (userID)
+            REFERENCES users (userID)
+);
+
+CREATE TABLE friendship(
+    userID1 INT PRIMARY KEY,
+    userID2 INT PRIMARY KEY,
+    timestamp VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_users
+        FOREIGN KEY (userID1)
+            REFERENCES users (userID),
+    CONSTRAINT fk_users
+        FOREIGN KEY (userID2)
+            REFERENCES users (userID)
+);
+
+CREATE TABLE Event(
+    eventID INT PRIMARY KEY,
+    userID INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    place VARCHAR(255) NOT NULL,
+    startDate DATE NOT NULL,
+    endDate DATE NOT NULL,
+    CONSTRAINT fk_users
+        FOREIGN KEY (userID)
+            REFERENCES users (userID)
+);
+
+CREATE TABLE Attendee(
+    eventID INT PRIMARY KEY,
+    userID INT PRIMARY KEY,
+    CONSTRAINT fk_users
+        FOREIGN KEY (userID)
+            REFERENCES users (userID),
+    CONSTRAINT fk_event
+        FOREIGN KEY (eventID)
+            REFERENCES Event (eventID)
+);
+
+CREATE TABLE Subscription(
+    subscriptionID INT PRIMARY KEY,
+    userID INT PRIMARY KEY,
+    dateOfPayment DATE NOT NULL,
+    paymentMethod  VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_users
+        FOREIGN KEY (userID)
+            REFERENCES users (userID)
 );
