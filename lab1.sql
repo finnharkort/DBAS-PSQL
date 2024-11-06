7b310cb0ddb65bb6d636457b48042a60a7b23e0a
Finn Harkort-Berge <finnhb@kth.se>
2024-11-06 12:33:25 +0100

    add post

--- lab1.sql
+++ lab1.sql
@@ -1,4 +1,9 @@
-CREATE TABLE smUser(
+CREATE TABLE post(
+    postID INT NOT NULL PRIMARY KEY,
+    
+)
+
+CREATE TABLE sm_user(
     userID INT NOT NULL PRIMARY KEY,
     fullName VARCHAR(255) NOT NULL
 );
