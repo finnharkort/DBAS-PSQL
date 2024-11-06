2436e8022a353c2a24f89a65a5d12b03a060ed3a
Finn Harkort-Berge <finnhb@kth.se>
2024-11-06 13:15:58 +0100

    test foreign constraints

--- lab1.sql
+++ lab1.sql
@@ -1,28 +1,36 @@
 DROP TABLE smuser;
 DROP TABLE textPost;
 
+CREATE TABLE sm_user(
+    userID INT PRIMARY KEY,
+    fullName VARCHAR(255)
+);
+
 CREATE TABLE Post(
-    postID INT NOT NULL PRIMARY KEY,
+    postID INT PRIMARY KEY,
     userID INT NOT NULL,
     title VARCHAR(255),
     date Date NOT NULL,
     place VARCHAR(255),
-    tag VARCHAR(255)[]
+    tag VARCHAR(255)[],
+    CONSTRAINT fk_sm_user
+        FOREIGN KEY (userID)
+            REFERENCES sm_user (userID)
 );
 
 CREATE TABLE TextPost(
-    postID INT NOT NULL REFERENCES,
+    postID INT PRIMARY KEY,
     textContent VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE ImagePost(
-    postID INT NOT NULL REFERENCES,
+    postID INT PRIMARY KEY,
     imageURL VARCHAR(255) NOT NULL,
     filter VARCHAR(255)[]
 );
 
 CREATE TABLE VideoPost(
-    postID INT NOT NULL REFERENCES,
+    postID INT PRIMARY KEY,
     videoURL VARCHAR(255) NOT NULL,
     codec VARCHAR(255)
 );
