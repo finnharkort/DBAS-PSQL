8788aaec2ca33fe4cf3534b56e5bd7d256530ff8
KickoKicko <kickokicko110@gmail.com>
2024-11-07 12:30:26 +0100

    add query commands file

---
+++ query.sql
@@ -0,0 +1,11 @@
+SELECT * FROM users;
+
+SELECT * FROM friendship;
+
+SELECT * FROM imagePost;
+SELECT * FROM videoPost;
+SELECT * FROM textPost;
+
+SELECT * FROM events;
+
+SELECT * FROM subscription;
