5aad349e1d8d493c569f62226e1d9fddb67d6e0b
Finn Harkort-Berge <finnharkort@gmail.com>
2024-11-07 13:34:18 +0100

    merge insert statements

--- insert.sql
+++ insert.sql
@@ -12,14 +12,11 @@ INSERT INTO friendship (userID1, userID2) VALUES (3,5);
 INSERT INTO friendship (userID1, userID2) VALUES (4,5);
 INSERT INTO friendship (userID1, userID2) VALUES (5,6);
 
-INSERT INTO post(postID, userID, title, date, place) VALUES (1, 3, 'På stranden :) Vad gör ni?', '2024-06-24', 'Tofta Beach');
-INSERT INTO videopost(postID, videoURL, codec) VALUES (1, 'https://vimeo.com/123456789', 'HEVC');
+INSERT INTO post(postID, userID, title, date, place, videoURL, codec) VALUES (1, 3, 'På stranden :) Vad gör ni?', '2024-06-24', 'Tofta Beach', 'https://vimeo.com/123456789', 'HEVC');
 
-INSERT INTO post(postID, userID, title, date, place) VALUES (2, 1, 'Thoughts on the election', '2024-08-01', NULL);
-INSERT INTO textpost(postID, textContent) VALUES (2, 'What are your thoughts on the election in America? I feel torn.');
+INSERT INTO post(postID, userID, title, date, place, textContent) VALUES (2, 1, 'Thoughts on the election', '2024-08-01', NULL, 'What are your thoughts on the election in America? I feel torn.');
 
-INSERT INTO post(postID, userID, title, date, place) VALUES (3, 6, 'Pluggar...', '2024-10-15', 'D-Huset');
-INSERT INTO imagePost(postID, imageURL, filter) VALUES (3, 'https://i.imgur.com/Tv8MzJk.jpg', NULL);
+INSERT INTO imagepost(postID, userID, title, date, place, imageURL, filter) VALUES (3, 6, 'Pluggar...', '2024-10-15', 'D-Huset', 'https://i.imgur.com/Tv8MzJk.jpg', NULL);
 
 INSERT INTO posttag(postID, tag) VALUES (1, 'social');
 INSERT INTO posttag(postID, tag) VALUES (1, 'question');
