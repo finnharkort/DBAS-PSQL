4be84388765f02f12ca38628fed9a4d895b00db4
Finn Harkort-Berge <finnharkort@gmail.com>
2024-11-07 13:23:21 +0100

    fix bug

--- insert.sql
+++ insert.sql
@@ -12,14 +12,14 @@ INSERT INTO friendship (userID1, userID2) VALUES (3,5);
 INSERT INTO friendship (userID1, userID2) VALUES (4,5);
 INSERT INTO friendship (userID1, userID2) VALUES (5,6);
 
-INSERT INTO post(postID, userID, title, date, place) VALUES (1, 3, 'På stranden :) Vad gör ni?', '2024-06-24');
+INSERT INTO post(postID, userID, title, date, place) VALUES (1, 3, 'På stranden :) Vad gör ni?', '2024-06-24', 'Tofta Beach');
 INSERT INTO videopost(postID, videoURL, codec) VALUES (1, 'https://vimeo.com/123456789', 'HEVC');
 
-INSERT INTO post(postID, userID, title, date, place) VALUES (2, 1, NULL, '2024-08-01');
-INSERT INTO imagePost(postID, imageURL, filter) VALUES (2, 'https://i.imgur.com/dQw4w9W.jpg', 'sepia');
+INSERT INTO post(postID, userID, title, date, place) VALUES (2, 1, 'Thoughts on the election', '2024-08-01');
+INSERT INTO textpost(postID, imageURL, filter) VALUES (2, 'https://i.imgur.com/dQw4w9W.jpg', 'sepia', NULL);
 
-INSERT INTO post(postID, userID, title, date, place) VALUES (3, 6, 'Pluggar...', '2024-10-15');
-INSERT INTO imagePost(postID, imageURL, filter) VALUES (3, 'https://i.imgur.com/Tv8MzJk.jpg', NULL);
+INSERT INTO post(postID, userID, title, date, place) VALUES (3, 6, 'Pluggar...', '2024-10-15', NULL);
+INSERT INTO imagePost(postID, textContent) VALUES (3, 'Whate are your thoughts on the election in America? I feel torn.');
 
 INSERT INTO posttag(postID, tag) VALUES (1, 'social');
 INSERT INTO posttag(postID, tag) VALUES (1, 'question');
@@ -27,10 +27,10 @@ INSERT INTO posttag(postID, tag) VALUES (3, 'studying');
 
 INSERT INTO events(eventID, userID, title, place, startDate, endDate) VALUES (1, 1, 'Tentapub', 'Nymble', '2024-10-20', '2024-10-21');
 
-INSERT INTO attende(eventID, userID) VALUES (1,1);
-INSERT INTO attende(eventID, userID) VALUES (1,2);
-INSERT INTO attende(eventID, userID) VALUES (1,3);
-INSERT INTO attende(eventID, userID) VALUES (1,6);
+INSERT INTO attendee(eventID, userID) VALUES (1,1);
+INSERT INTO attendee(eventID, userID) VALUES (1,2);
+INSERT INTO attendee(eventID, userID) VALUES (1,3);
+INSERT INTO attendee(eventID, userID) VALUES (1,6);
 
 INSERT INTO subscription(subscriptionID, userID, dateOfPayment, paymentMethod) VALUES (1,1,'2024-10-03','Klarna');
 INSERT INTO subscription(subscriptionID, userID, dateOfPayment, paymentMethod) VALUES (2,2,'2024-10-07','Swish');
