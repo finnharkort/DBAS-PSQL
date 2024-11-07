e7b3ac2df7445b3f30d28a1360effd00538c9512
Finn Harkort-Berge <finnharkort@gmail.com>
2024-11-07 14:25:50 +0100

    change CREATE TABLE statmeents

--- lab1p+.sql
+++ lab1p+.sql
@@ -1,7 +1,10 @@
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS worksAt;
+DROP TABLE IF EXISTS treating;
+DROP TABLE IF EXISTS mentor;
 DROP TABLE IF EXISTS doctor;
 DROP TABLE IF EXISTS nurse;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS employee;
 DROP TABLE IF EXISTS patient;
 
 
@@ -11,31 +14,59 @@ CREATE TABLE department(
 );
 
 CREATE TABLE employee(
-    iD INT PRIMARY KEY,
+    ID INT PRIMARY KEY,
     phoneNr INT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    mentor INT --another employees id
+    name VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE worksAt(
+    employeeID INT,
+    departmentName,
+    startDate DATE,
+    PRIMARY KEY(employeeID,departmentName),
+    CONSTRAINT fk_employee
+        FOREIGN KEY (employeeID)
+            REFERENCES employee (ID)
+)
+
+CREATE TABLE mentor(
+    apprenticeID INT PRIMARY KEY,
+    mentorID INT,
+    CONSTRAINT fk_employee1
+        FOREIGN KEY (apprenticeID)
+            REFERENCES employee (ID),
+    CONSTRAINT fk_employee2
+        FOREIGN KEY (mentorID)
+            REFERENCES employee (ID)
+)
 
 CREATE TABLE doctor(
-    iD INT PRIMARY KEY,
     roomNr INT NOT NULL,
-    specialization VARCHAR(255) NOT NULL,
-    patient INT[] --patient id
-);
+    specialization VARCHAR(255) NOT NULL
+) INHERITS (employee);
 
 CREATE TABLE nurse(
-    iD INT PRIMARY KEY,
     degree VARCHAR(255)
-);
+) INHERITS (employee);
 
 CREATE TABLE patient(
-    iD INT PRIMARY KEY,
-    diagnosis VARCHAR(255), --Nullable
+    ID INT PRIMARY KEY,
+    diagnosises VARCHAR(255)[], --Nullable
     name VARCHAR(255) NOT NULL,
-    age INT NOT NULL,
-    doctors INT[]
+    age INT NOT NULL
 );
+
+CREATE TABLE treating(
+    doctorID INT,
+    patientID INT,
+    PRIMARY KEY (doctorID, patientID),
+    CONSTRAINT
+        FOREIGN KEY (doctorID)
+            REFERENCES doctor (ID),
+    CONSTRAINT
+        FOREIGN KEY (patientID)
+            REFERENCES patient (ID),
+)
 
 
 GRANT ALL PRIVILEGES ON TABLE department TO nikolajg;
@@ -43,3 +74,5 @@ GRANT ALL PRIVILEGES ON TABLE employee TO nikolajg;
 GRANT ALL PRIVILEGES ON TABLE doctor TO nikolajg;
 GRANT ALL PRIVILEGES ON TABLE nurse TO nikolajg;
 GRANT ALL PRIVILEGES ON TABLE patient TO nikolajg;
+GRANT ALL PRIVILEGES ON TABLE worksAt TO nikolajg;
+GRANT ALL PRIVILEGES ON TABLE treating TO nikolajg;
