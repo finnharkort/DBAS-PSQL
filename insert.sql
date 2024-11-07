80c063d2c009c76b8d9a8330de3b87c580ee45f3
Finn Harkort-Berge <finnharkort@gmail.com>
2024-11-07 12:16:46 +0100

    create 6 users and 4 friendships

---
+++ insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO users (userID, fullName) VALUES (1, 'Anna');
+INSERT INTO users (userID, fullName) VALUES (2, 'Bosse');
+INSERT INTO users (userID, fullName) VALUES (3, 'Carl');
+INSERT INTO users (userID, fullName) VALUES (4, 'David');
+INSERT INTO users (userID, fullName) VALUES (5, 'Eva');
+INSERT INTO users (userID, fullName) VALUES (6, 'Fredrik');
+
+INSERT INTO friendship (userID1, userID2) VALUES (1,2);
+INSERT INTO friendship (userID1, userID2) VALUES (3,4);
+INSERT INTO friendship (userID1, userID2) VALUES (4,5);
+INSERT INTO friendship (userID1, userID2) VALUES (5,6);
+
