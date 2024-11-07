ace93c5e0d7ce311947d14c1b89f96acaf2632d7
Finn Harkort-Berge <finnharkort@gmail.com>
2024-11-07 13:27:18 +0100

    fix bug

--- insert.sql
+++ insert.sql
@@ -15,11 +15,11 @@ INSERT INTO friendship (userID1, userID2) VALUES (5,6);
 INSERT INTO post(postID, userID, title, date, place) VALUES (1, 3, 'På stranden :) Vad gör ni?', '2024-06-24', 'Tofta Beach');
 INSERT INTO videopost(postID, videoURL, codec) VALUES (1, 'https://vimeo.com/123456789', 'HEVC');
 
-INSERT INTO post(postID, userID, title, date, place) VALUES (2, 1, 'Thoughts on the election', '2024-08-01');
-INSERT INTO textpost(postID, imageURL, filter) VALUES (2, 'https://i.imgur.com/dQw4w9W.jpg', 'sepia', NULL);
+INSERT INTO post(postID, userID, title, date, place) VALUES (2, 1, 'Thoughts on the election', '2024-08-01', NULL);
+INSERT INTO textpost(postID, textContent) VALUES (2, 'What are your thoughts on the election in America? I feel torn.');
 
-INSERT INTO post(postID, userID, title, date, place) VALUES (3, 6, 'Pluggar...', '2024-10-15', NULL);
-INSERT INTO imagePost(postID, textContent) VALUES (3, 'Whate are your thoughts on the election in America? I feel torn.');
+INSERT INTO post(postID, userID, title, date, place) VALUES (3, 6, 'Pluggar...', '2024-10-15', 'D-Huset');
+INSERT INTO imagePost(postID, textContent) VALUES (3, 'https://i.imgur.com/Tv8MzJk.jpg', NULL);
 
 INSERT INTO posttag(postID, tag) VALUES (1, 'social');
 INSERT INTO posttag(postID, tag) VALUES (1, 'question');
