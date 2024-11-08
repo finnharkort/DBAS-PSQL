2c63c2bbd062c1a2d77582f72c88d91658af07f8
Finn Harkort-Berge <finnharkort@gmail.com>
2024-11-08 14:04:58 +0100

    test

--- insert.sql
+++ insert.sql
@@ -25,13 +25,13 @@ INSERT INTO friendship (userID1, userID2) VALUES (5,6);
 
 ------FOR SECOND ALT-------------
 
-INSERT INTO post(postID) VALUES (1)
+INSERT INTO post(postID) VALUES (1);
 INSERT INTO videopost(postID, userID, title, date, place, videoURL, codec) VALUES (1, 3, 'På stranden :) Vad gör ni?', '2024-06-24', 'Tofta Beach', 'https://vimeo.com/123456789', 'HEVC');
 
-INSERT INTO post(postID) VALUES (2)
+INSERT INTO post(postID) VALUES (2);
 INSERT INTO textpost(postID, userID, title, date, place, textContent) VALUES (2, 1, 'Thoughts on the election', '2024-08-01', NULL, 'What are your thoughts on the election in America? I feel torn.');
 
-INSERT INTO post(postID) VALUES (3)
+INSERT INTO post(postID) VALUES (3);
 INSERT INTO imagepost(postID, userID, title, date, place, imageURL, filter) VALUES (3, 6, 'Pluggar...', '2024-10-15', 'D-Huset', 'https://i.imgur.com/Tv8MzJk.jpg', NULL);
 
 
