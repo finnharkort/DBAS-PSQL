9a66d403945768fec256927551fc8189b107633a
Finn Harkort-Berge <finnharkort@gmail.com>
2024-11-08 13:28:58 +0100

    test

--- insert.sql
+++ insert.sql
@@ -12,7 +12,9 @@ INSERT INTO friendship (userID1, userID2) VALUES (3,5);
 INSERT INTO friendship (userID1, userID2) VALUES (4,5);
 INSERT INTO friendship (userID1, userID2) VALUES (5,6);
 
-INSERT INTO post(postID, userID, title, date, place) VALUES (1, 2, 'asddas', '2024-01-05', 'sdfs');
+INSERT INTO post(postID, userID, title, date, place) VALUES (1, 3, 'På stranden :) Vad gör ni?', '2024-06-24', 'Tofta Beach');
+INSERT INTO post(postID, userID, title, date, place) VALUES (2, 1, 'Thoughts on the election', '2024-08-01', NULL);
+INSERT INTO post(postID, userID, title, date, place) VALUES (3, 6, 'Pluggar...', '2024-10-15', 'D-Huset');
 
 INSERT INTO videopost(postID, userID, title, date, place, videoURL, codec) VALUES (1, 3, 'På stranden :) Vad gör ni?', '2024-06-24', 'Tofta Beach', 'https://vimeo.com/123456789', 'HEVC');
 
